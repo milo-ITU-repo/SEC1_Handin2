5a649378605955ef06de1dd75cbbcb754ab16705
Mille Mei Zhen Loo <milo@itu.dk>
2022-10-23 19:02:48 +0200

    made functional server/client structure

--- client.py
+++ client.py
@@ -1,16 +1,38 @@
 import random
 import socket
+import ssl
 import commitment as c
+import primes as p
+
+HEADER = 64
+PORT = 6969
+#SERVER = "192.168.50.191"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER,PORT)
+FORMAT = 'utf-8'
+DISCONNEC_MESSAGE = "DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# setting a TLS wrapper socket using the ssl library
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client = ssl.wrap_socket(client, keyfile="./CA/client.key", certfile="./CA/client.crt")
+
+client.connect(ADDR)
 
 
-
-
-
-    
-
-
-
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_lenght = str(msg_length).encode(FORMAT)
+    send_lenght += b' ' * (HEADER - len(send_lenght)) # padding the message to be the correct lenght
+    client.send(send_lenght)
+    client.send(message)
+    print("You :", msg)
 
 
 if __name__== "__main__":
-    c.is_prime()
+
+    send("hello")
+    send("hello again")
+    send("DISCONNECT")
--- server.py
+++ server.py
@@ -1,10 +1,66 @@
+# Socket code is partially from https://www.youtube.com/watch?v=3QiPPX-KeSc
+
 import random
 import socket
+from sqlite3 import connect
+import ssl
+import threading
 import commitment as c
+import primes as p
+
+HEADER = 64
+PORT = 6969
+#SERVER = "192.168.50.191"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER,PORT)
+FORMAT = 'utf-8'
+DISCONNEC_MESSAGE = "DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # we stream data through the socket 
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server = ssl.wrap_socket(
+    server, server_side=True, keyfile="./CA/server.key", certfile="./CA/server.crt"
+)
+
+server.bind(ADDR)
 
 
 
 
+
+
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+            if msg == DISCONNEC_MESSAGE :
+                connected = False
+
+            print(f"[{addr}]: {msg}")
+            conn.send("MSG recieved".encode(FORMAT))
+    
+    conn.close()
+
+
+def start():
+    server.listen()
+
+    print(f"[LISTENING] Server is listening on {SERVER}") 
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
     
 
@@ -13,4 +69,5 @@ import commitment as c
 
 
 if __name__== "__main__":
-    c.is_prime()
+    print("Starting server...")
+    start()
